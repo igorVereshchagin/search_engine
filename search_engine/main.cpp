e08527ea99402bc7eb922729c15aa7b84e9400a5
Igor Vereshchagin <igor.a.vereshchagin@gmail.com>
2022-09-21 18:26:41 +0300

    gtest added

--- search_engine/main.cpp
+++ search_engine/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "ConverterJSON.h"
 
+#include "gtest/gtest.h"
+TEST(sample_test_case, sample_test)
+{
+EXPECT_EQ(1, 1);
+}
+
 int main()
 {
+  ::testing::InitGoogleTest();
+  RUN_ALL_TESTS();
   ConverterJSON conv;
   std::vector<std::string> list = conv.GetTextDocuments();
   return 0;
