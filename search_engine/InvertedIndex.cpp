ca32a218d5ea4fa313f3827e4e93728c45c7e6d4
Igor <igor.a.vereshchagin@gmail.com>
2022-10-10 06:28:44 +0300

    inverted index added

---
+++ search_engine/InvertedIndex.cpp
@@ -0,0 +1,17 @@
+//
+// Created by igora on 09.10.2022.
+//
+
+#include "InvertedIndex.h"
+
+void InvertedIndex::UpdateDocumentBase(std::vector<std::string> inputDocs)
+{
+
+}
+
+std::vector<Entry> InvertedIndex::GetWordCount(const std::string& word)
+{
+  std::vector<Entry> ret;
+  ret.clear();
+  return ret;
+}
---
+++ search_engine/InvertedIndex.h
@@ -0,0 +1,40 @@
+//
+// Created by igora on 09.10.2022.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+struct Entry {
+  size_t docId, count;
+  // Данный оператор необходим для проведения тестовых сценариев
+  bool operator == (const Entry& other) const {
+    return (docId == other.docId && count == other.count);
+  }
+};
+
+class InvertedIndex
+{
+public:
+  InvertedIndex() = default;
+/**
+* Обновить или заполнить базу документов, по которой будем совершать поиск
+* @param texts_input содержимое документов
+*/
+  void UpdateDocumentBase(std::vector<std::string> inputDocs);
+/**
+  * Метод определяет количество вхождений слова word в загруженной базе документов
+  * @param word слово, частоту вхождений которого необходимо определить
+  * @return возвращает подготовленный список с частотой слов
+  */
+  std::vector<Entry> GetWordCount(const std::string& word);
+
+private:
+  std::vector<std::string> docs;
+  std::map<std::string, std::vector<Entry>> freqDictionary
+};
+
+
