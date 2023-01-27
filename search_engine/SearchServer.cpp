ab629c1dbfb9b92b9766bd89ac67d94c62b381f6
Igor <igor.a.vereshchagin@gmail.com>
2023-01-27 22:37:17 +0400

    debugger is working

--- search_engine/SearchServer.cpp
+++ search_engine/SearchServer.cpp
@@ -1,7 +1,20 @@
 #include "SearchServer.h"
 
+#include <sstream>
+
 std::vector<std::vector<RelativeIndex>> SearchServer::search(const std::vector<std::string>& queries_input)
 {
   std::vector<std::vector<RelativeIndex>> ret;
+  for (auto it = queries_input.begin(); it != queries_input.end(); it++)
+  {
+    std::stringstream ss(*it);
+    std::string word;
+    std::vector<std::pair<std::string, std::vector<Entry>>> entries;
+    while (!ss.eof())
+    {
+      ss >> word;
+      entries.push_back(std::make_pair(word, _index.GetWordCount(word)));
+    }
+  }
   return ret;
 }
